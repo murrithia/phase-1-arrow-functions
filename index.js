7fa75ed98c100155f5fbaf9f2aa08bb72e96fa79
Allan <allanmuriithi441@gmail.com>
2024-03-27 05:46:22 -0400

    arrow function

---
+++ index.js
@@ -0,0 +1,12 @@
+let a = 2000;
+let b = 100;
+// The divide arrow function
+const divide = (a,b) => a/b;
+console.log(divide);
+// The square arrow function
+const square = (x) => x ** 2;
+console.log(square);
+
+// The add arrow function
+const add = (m,n) => m+n;
+console.log(add);
